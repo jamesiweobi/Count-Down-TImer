844eb1be7256fbf5d980e3a235d80ea1f1264c3e
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-05-08 22:23:01 +0100

    Added the date count down and refresh interval

--- app.js
+++ app.js
@@ -32,3 +32,41 @@ const hours = futureDate.getHours();
 giveaway.textContent = `Give away ends on ${year}, ${
   months[futureDate.getMonth()]
 } ${weekdays[futureDate.getDay()]} ${futureDate.getDay()}, 00:00am`;
+
+// future time in ms
+
+const futureTime = futureDate.getTime();
+
+const getRemainingTime = () => {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60mins
+  // 1d = 24hrs
+
+  // Values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  const day = Math.floor(t / oneDay);
+  const hour = Math.floor((t % oneDay) / oneHour);
+  const minute = Math.floor((t % oneHour) / oneMinute);
+  const sec = Math.floor((t % oneMinute) / 1000);
+
+  const values = [day, hour, minute, sec];
+  const format = (item) => {
+    if (item < 10) return `0${item}`;
+    return item;
+  };
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countDown);
+    deadline.innerHTML = `<h4 class="expired">Happy Birthday James Iweobi</h4>`;
+  }
+};
+let countDown = setInterval(getRemainingTime, 1000);
+getRemainingTime();
